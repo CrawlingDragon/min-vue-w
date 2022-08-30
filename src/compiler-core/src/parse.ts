ba5231a89e4a03abdc27330e634da7bffef4e82e
wuhelong <5387348+wuhelong@user.noreply.gitee.com>
2022-08-30 15:32:58 +0800

    实现解析三种联合类型

--- src/compiler-core/src/parse.ts
+++ src/compiler-core/src/parse.ts
@@ -6,8 +6,9 @@ const enum TagType {
 
 export function baseParse(content) {
   let context = createParseContent(content);
-  return createRoot(parseChildren(context));
+  return createRoot(parseChildren(context, []));
 }
+
 function parseInterpolation(context) {
   let openDelimiter = '{{';
   let closeDelimiter = '}}';
@@ -26,8 +27,7 @@ function parseInterpolation(context) {
   content = content.trim();
 
   //继续切割content,为后面继续处理content
-  context.source = context.source.slice(0, rawContentLength + closeDelimiter.length);
-
+  advanceBy(context, rawContentLength + closeDelimiter.length);
   return {
     type: NodeTypes.INTERPOLATION,
     content: {
@@ -36,43 +36,102 @@ function parseInterpolation(context) {
     },
   };
 }
-function parseChildren(context) {
+function parseChildren(context, ancestors) {
   let nodes: any = [];
-  let node;
-  let source = context.source;
 
-  if (source.startsWith('{{')) {
-    // 处理插槽类型
-    node = parseInterpolation(context);
-  } else if (source[0] === '<') {
-    // 处理element 类型
-    // 匹配<div></div> 的<
-    if (/[a-z]/i.test(source[1])) {
-      //匹配<div></div> 的 d 类型
-      node = parseElement(context);
+  while (!isEnd(context, ancestors)) {
+    let node;
+    let source = context.source;
+
+    if (source.startsWith('{{')) {
+      // 处理插槽类型
+      node = parseInterpolation(context);
+    } else if (source[0] === '<') {
+      // 处理element 类型
+      // 匹配<div></div> 的<
+      if (/[a-z]/i.test(source[1])) {
+        //匹配<div></div> 的 d 类型
+        node = parseElement(context, ancestors);
+      }
     }
+    if (!node) {
+      node = parseText(context);
+    }
+    nodes.push(node);
   }
-  nodes.push(node);
   return nodes;
 }
 
-function parseElement(context) {
+function isEnd(context, ancestors) {
+  // source 有值的时候
+  // 或者 碰到结束标签的时候
+  let s = context.source;
+  if (s.startsWith('</')) {
+    for (let i = 0; i < ancestors.length; i++) {
+      let tag = ancestors[i].tag; // 收集数组内的 tag
+      // let sourceTag = s.slice(2, 2 + tag.length); // source的</div>
+      if (startsWithEndTagOpen(s, tag) === tag) {
+        return true;
+      }
+    }
+  }
+
+  return !s;
+}
+function parseText(context) {
+  let content = context.source;
+
+  let endIndex = content.length;
+  let endTokens = ['{{', '</'];
+
+  for (let i = 0; i < endTokens.length; i++) {
+    let index = content.indexOf(endTokens[i]);
+    if (index !== -1 && index < endIndex) {
+      // index <  endIndex ：选择靠左的那个标签
+      endIndex = index;
+    }
+  }
+
+  // 提取text 内容
+  content = context.source.slice(0, endIndex);
+
+  // 删除context内容
+  advanceBy(context, endIndex);
+
+  return {
+    type: NodeTypes.TEXT,
+    content: content,
+  };
+}
+function parseElement(context, ancestors) {
   // 具体处理 element类型的函数
   // 1：判断出是不是 element 类型，上面已经处理
   // 2: 通过正则匹配<div> 和结束标签</div>
   // 3：删除处理完的context
+  let element: any = parseTag(context, TagType.START);
+  let tag = element.tag;
 
-  let element = parseTag(context, TagType.START);
+  ancestors.push(element); // 收集element标签
+
+  let children = parseChildren(context, ancestors);
+  element.children = children;
+
+  ancestors.pop(); //在处理完element标签后，弹出末尾
   parseTag(context, TagType.END);
-  console.log('context.source', context.source);
+  if (startsWithEndTagOpen(context.source, tag) !== tag) {
+    throw new Error('缺少标签：' + tag);
+  }
+
   return element;
 }
 function parseTag(context, type) {
   // exec 匹配一遍目标元素，返回一个匹配的数组  [ '<div', 'div', index: 0, input: '<div></div>', groups: undefined ]
   let match: any = /^<\/?([a-z]*)/i.exec(context.source);
+
   let tag = match[1];
   advanceBy(context, match[0].length);
   advanceBy(context, 1);
+
   if (type === TagType.END) return;
   return {
     type: NodeTypes.ELEMENT,
@@ -90,4 +149,8 @@ function createParseContent(content) {
 
 function advanceBy(context, length) {
   context.source = context.source.slice(length);
+}
+
+function startsWithEndTagOpen(source, tag) {
+  return source.slice(2, 2 + tag.length);
 }
--- src/compiler-core/test/parse.spec.ts
+++ src/compiler-core/test/parse.spec.ts
@@ -18,6 +18,71 @@ describe('Parse', () => {
     expect(ast.children[0]).toStrictEqual({
       type: NodeTypes.ELEMENT,
       tag: 'div',
+      children: [],
     });
+  });
+  it('parse text', () => {
+    const ast = baseParse('some text');
+    expect(ast.children[0]).toStrictEqual({
+      type: NodeTypes.TEXT,
+      content: 'some text',
+    });
+  });
+
+  test('parse all', () => {
+    const ast = baseParse('<div>hi,{{message}}</div>');
+    expect(ast.children[0]).toStrictEqual({
+      type: NodeTypes.ELEMENT,
+      tag: 'div',
+      children: [
+        {
+          type: NodeTypes.TEXT,
+          content: 'hi,',
+        },
+        {
+          type: NodeTypes.INTERPOLATION,
+          content: {
+            type: NodeTypes.SIMPLE_EXPRESSION,
+            content: 'message',
+          },
+        },
+      ],
+    });
+  });
+
+  test('nested element', () => {
+    const ast = baseParse('<div><p>hi</p>{{message}}</div>');
+    expect(ast.children[0]).toStrictEqual({
+      type: NodeTypes.ELEMENT,
+      tag: 'div',
+      children: [
+        {
+          type: NodeTypes.ELEMENT,
+          tag: 'p',
+          children: [
+            {
+              type: NodeTypes.TEXT,
+              content: 'hi',
+            },
+          ],
+        },
+        {
+          type: NodeTypes.INTERPOLATION,
+          content: {
+            type: NodeTypes.SIMPLE_EXPRESSION,
+            content: 'message',
+          },
+        },
+      ],
+    });
+  });
+
+  test.only('should throw error when lock and tag', () => {
+    // 实现步骤
+    // 1： 对element标签收集，在isEnd内，依次判断收集的element标签
+    // 2: 在parseElement时，对起始标签和结束标签不一样的时候，
+    expect(() => {
+      baseParse('<div><span></div>');
+    }).toThrow('缺少标签：span');
   });
 });
